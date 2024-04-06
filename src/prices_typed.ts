5a46b314ac2b48a84018fa05c22e9067d45cbaaf
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2024-04-06 13:35:27 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -85,7 +85,8 @@ function createApp(database: Database) {
 
   const isTemporalHoliday = (date?: Temporal.PlainDate): boolean => {
     const holidays: Holiday[]  = database.getHolidays();
-    return !!(holidays.find((holiday: Holiday) => { const holidate =Temporal.PlainDate.from(holiday.holiday) === date }));
+    return !!(holidays.find((holiday: Holiday) => { const holidate =Temporal.PlainDate.from(holiday.holiday) === date }
+    ));
   }
 
   function isHoliday(date: Date | undefined) {
